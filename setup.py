867e1e56c7161e6156b2f03db2ca3ebb54574ba6
Francesco Sforazzini <francesco.sforazzini@gmail.com>
2019-08-14 11:56:57 +0200

    midify setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,6 @@ def install_deps():
     """Reads requirements.txt and preprocess it
     to be feed into setuptools.
 
-    This is the only possible way (we found)
-    how requirements.txt can be reused in setup.py
-    using dependencies from private github repositories.
-
-    Links must be appendend by `-{StringWithAtLeastOneNumber}`
-    or something like that, so e.g. `-9231` works as well as
-    `1.1.0`. This is ignored by the setuptools, but has to be there.
-
-    Warnings:
-        to make pip respect the links, you have to use
-        `--process-dependency-links` switch. So e.g.:
-        `pip install --process-dependency-links {git-url}`
-
     Returns:
          list of packages and dependency links.
     """
@@ -27,7 +14,7 @@ def install_deps():
     for resource in default:
         if 'git+https' in resource:
             pkg = resource.split('#')[-1]
-            links.append(resource.strip() + '-9876543210')
+            links.append(resource.strip())
             new_pkgs.append(pkg.replace('egg=', '').rstrip())
         else:
             new_pkgs.append(resource.strip())
@@ -44,8 +31,11 @@ setup(name='lung_segmentation',
       author_email='f.sforazzini@dkfz.de',
       license='Apache 2.0',
       zip_safe=False,
-      install_requires=pkgs,
-      dependency_links=new_links,
+      install_requires=['requests==2.22.0',
+                        'tensorflow==1.14.0',
+			],
+      dependency_links=['git+https://github.com/sforazz/basecore.git#egg=basecore',
+                        'git+https://github.com/sforazz/dl.git#egg=dl'],
       packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
       classifiers=[
           'Intended Audience :: Science/Research',
