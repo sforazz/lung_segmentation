92d270674fa56ccf6acf7098395603d13a9960f1
Francesco Sforazzini <francesco.sforazzini@gmail.com>
2019-10-15 14:53:13 +0200

    added jaccard distance loss

--- lung_segmentation/loss.py
+++ lung_segmentation/loss.py
@@ -1,6 +1,7 @@
 "Losses to be used for CNN training"
 from keras import backend as K
 
+
 def dice_coefficient(y_true, y_pred):
     "Dice coefficient loss"
     smoothing_factor = 1
@@ -10,6 +11,20 @@ def dice_coefficient(y_true, y_pred):
     return ((2.0 * intersection + smoothing_factor)
             /(K.sum(y_true_f) + K.sum(y_pred_f) + smoothing_factor))
 
+
 def loss_dice_coefficient_error(y_true, y_pred):
     "Dice coefficient"
     return -dice_coefficient(y_true, y_pred)
+
+
+def jaccard_distance(y_true, y_pred, smooth=100):
+    "Jaccard distance"
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return jac
+
+
+def jaccard_distance_loss(y_true, y_pred, smooth=100):
+    "Jaccard distance loss"
+    return (1 - jaccard_distance(y_true, y_pred)) * smooth
